f70387af95d16e25d915b9a1cb06a1c0e9a08554
KirbyKidJ <70983335+KirbyKid256@users.noreply.github.com>
2025-02-03 10:42:56 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# BangerScript
+
+<img src="icon.png" alt="BangerScript Icon" width="128"/>
+
+A relatively simple Godot 4 Plugin to load external GDScripts to use for modding. I made this to use in my source mod of Beat Banger. It mostly contains helper functions and will require knowledge in GDScript to use. For modders, I recommend using [Godot RE Tools](https://github.com/GDRETools/gdsdecomp) to decompile the source so you can use it alongside your softcoded scripts unless a wiki exists.
+
+## Implimentation Example
+
+This is an example for programmers on how to impliment the script loader. This should either be used on a different singleton so it doesn't get called again, or in a scene if you want to remove and re-add the scripts per scene.
+
+```gdscript
+signal a_signal()
+
+func _ready():
+	for script in BangerScript.get_scripts_at("res://scripts"):
+		BangerScript.add_script("res://scripts".path_join(script), true #if you're addings scripts globally across the game)
+
+	for node in BangerScript.get_children():
+		BangerScript.connect_node(node, "_on_a_signal", a_signal)
+		BangerScript.connect_node(node, "_on_b_signal", SingleTon.b_signal)
+
+
+func _exit_tree():
+	# Not sure if this is needed, but I prefer it to not clog the signal list
+	for node in BangerScript.get_children():
+		BangerScript.disconnect_node(node, "_on_a_signal", a_signal)
+
+	# Definitally needed for the root scene script, removes all non-global scripts
+	BangerScript.clear_scripts()
+```
---
+++ addons/bangerscript/BangerScript.gd
@@ -0,0 +1,85 @@
+extends Node
+
+var variables: Dictionary
+
+## Easy way to get a list of [GDScript] files from a given location.
+func get_scripts_at(path: String) -> Array:
+	var array: Array = DirAccess.get_files_at(path)
+	return array.filter(func(a): return a.get_extension() == "gd")
+
+## Adds a script to the current scene from the given location.
+func add_script(path: String, global: bool = false):
+	if not FileAccess.file_exists(path): return
+	for node in get_children(): if node.scene_file_path == path: return
+
+	var node: Node = Node.new()
+	node.scene_file_path = path
+	node.name = path.get_file().get_basename().to_pascal_case() + ("@Global" if global else "")
+	node.set_script(load(path))
+	add_child(node)
+
+	return node
+
+## Returns the index of the child using the given script in this class.
+func find_script(path: String) -> int:
+	for node in get_children(): if node.scene_file_path == path:
+		return node.get_index()
+	return -1
+
+## Removes a script from the current scene. However, it can still be used or re-added if you store it in a variable.
+func remove_script(path: String):
+	for node in get_children(): if node.scene_file_path == path:
+		remove_child(node)
+		return node
+
+## Completely removes a script from the current scene if it is running. After using this, you'll need to use `add_script` to add it back to the scene.
+func free_script(path: String):
+	for node in get_children(): if node.scene_file_path == path:
+		node.queue_free()
+		break
+
+## Easy way to free all the loaded scripts. Does not remove Global scripts automatically.
+func clear_scripts():
+	for node in get_children():
+		if node.name.ends_with("_Global"): continue
+		node.queue_free()
+
+## Sets a global variable from a script.
+func set_var(n: String, value, path: String):
+	for node in get_children(): if node.scene_file_path == path:
+		if not variables.has(path): variables[path] = {}
+		variables[path][n] = value
+
+## Gets a global variable from a script.
+func get_var(n: String, path: String, fallback = null) -> Variant:
+	for node in get_children(): if node.scene_file_path == path:
+		if not variables.has(path): break
+		return variables[path].get(n, fallback)
+	return fallback
+
+## Gets a global variable from a script, or adds it if the variable doesn't exist.
+func get_or_add_var(n: String, path: String, fallback = null) -> Variant:
+	for node in get_children(): if node.scene_file_path == path:
+		if not variables.has(path): variables[path] = {}
+		return variables[path].get_or_add(n, fallback)
+	return fallback
+
+## Removes a global variable assigned by a script.
+func remove_var(n: String, path: String):
+	for node in get_children(): if node.scene_file_path == path:
+		if not variables.has(path): break
+		variables[path].erase(n)
+		if variables[path].is_empty(): variables.erase(path)
+		break
+
+## Checks if a given method exists and if that method is connected to a signal within a node. If so, it applies the connection.
+func connect_node(node: Node, method: String, sigma: Signal) -> bool:
+	if node.has_method(method) and !sigma.is_connected(node[method]):
+		sigma.connect(node[method]); return true
+	return false
+
+## Checks if a given method exists and if that method is connected to a signal within a node. If so, it disconnects the method and signal.
+func disconnect_node(node: Node, method: String, sigma: Signal) -> bool:
+	if node.has_method(method) and sigma.is_connected(node[method]):
+		sigma.disconnect(node[method]); return true
+	return false
