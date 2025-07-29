c876a57eb13bce4159f1a82f889bc88b2decb9f6
KirbyKidJ <70983335+KirbyKid256@users.noreply.github.com>
2025-07-28 22:55:41 -0700

    In-game SceneTree viewer. This cause lagging
    - Moved Scripts to an internal node
    - Prevented GDScript data being cached
    - Added a Debug Overlay to show and select all the nodes in the root

--- addons/bangerscript/BangerScript.gd
+++ addons/bangerscript/BangerScript.gd
@@ -1,6 +1,17 @@
 extends Node
 
+@onready var scripts: Node
+
 var variables: Dictionary
+
+func _init() -> void:
+	scripts = Node.new()
+	scripts.name = "GDScripts"
+	add_child(scripts)
+
+func _ready() -> void:
+	if OS.is_debug_build():
+		add_child(preload("res://addons/bangerscript/debug/DebugOverlay.tscn").instantiate())
 
 ## Easy way to get a list of [GDScript] files from a given location.
 func get_scripts_at(path: String) -> Array:
@@ -11,69 +22,69 @@ func get_scripts_at(path: String) -> Array:
 ## Adds a script to the current scene from the given location.
 func add_script(path: String, global: bool = false):
 	if not FileAccess.file_exists(path): return
-	for node in get_children(): if node.scene_file_path == path: return
+	for node in scripts.get_children(): if node.scene_file_path == path: return
 
 	var node: Node = Node.new()
 	node.scene_file_path = path
 	node.name = path.get_file().get_basename().to_pascal_case() + ("@Global" if global else "")
-	node.set_script(load(path))
-	add_child(node)
+	node.set_script(ResourceLoader.load(path, "GDScript", ResourceLoader.CACHE_MODE_IGNORE))
+	scripts.add_child(node)
 
 	return node
 
 ## Checks if a script exists at the given path.
 func has_script(path: String) -> bool:
-	for node in get_children(): if node.scene_file_path == path:
+	for node in scripts.get_children(): if node.scene_file_path == path:
 		return true
 	return false
 
 ## Returns the index of the child using the given script in this class.
 func find_script(path: String) -> int:
-	for node in get_children(): if node.scene_file_path == path:
+	for node in scripts.get_children(): if node.scene_file_path == path:
 		return node.get_index()
 	return -1
 
 ## Removes a script from the current scene. However, it can still be used or re-added if you store it in a variable.
 func remove_script(path: String):
-	for node in get_children(): if node.scene_file_path == path:
-		remove_child(node)
+	for node in scripts.get_children(): if node.scene_file_path == path:
+		scripts.remove_child(node)
 		return node
 
 ## Completely removes a script from the current scene if it is running. After using this, you'll need to use `add_script` to add it back to the scene.
 func free_script(path: String):
-	for node in get_children(): if node.scene_file_path == path:
+	for node in scripts.get_children(): if node.scene_file_path == path:
 		node.queue_free()
 		break
 
 ## Easy way to free all the loaded scripts. Does not remove Global scripts automatically unless you specify to do so.
 func clear_scripts(remove_globals: bool = false):
-	for node in get_children():
+	for node in scripts.get_children():
 		if not remove_globals and node.name.ends_with("_Global"): continue
 		node.queue_free()
 
 ## Sets a global variable from a script.
 func set_var(n: String, value, path: String):
-	for node in get_children(): if node.scene_file_path == path:
+	for node in scripts.get_children(): if node.scene_file_path == path:
 		if not variables.has(path): variables[path] = {}
 		variables[path][n] = value
 
 ## Gets a global variable from a script.
 func get_var(n: String, path: String, fallback = null) -> Variant:
-	for node in get_children(): if node.scene_file_path == path:
+	for node in scripts.get_children(): if node.scene_file_path == path:
 		if not variables.has(path): break
 		return variables[path].get(n, fallback)
 	return fallback
 
 ## Gets a global variable from a script, or adds it if the variable doesn't exist.
 func get_or_add_var(n: String, path: String, fallback = null) -> Variant:
-	for node in get_children(): if node.scene_file_path == path:
+	for node in scripts.get_children(): if node.scene_file_path == path:
 		if not variables.has(path): variables[path] = {}
 		return variables[path].get_or_add(n, fallback)
 	return fallback
 
 ## Removes a global variable assigned by a script.
 func remove_var(n: String, path: String):
-	for node in get_children(): if node.scene_file_path == path:
+	for node in scripts.get_children(): if node.scene_file_path == path:
 		if not variables.has(path): break
 		variables[path].erase(n)
 		if variables[path].is_empty(): variables.erase(path)
---
+++ addons/bangerscript/debug/debug_overlay.gd
@@ -0,0 +1,108 @@
+extends CanvasLayer
+
+## Toggles the visibility of the BangerScript Debug Overlay. This can be changed to any keybind that fits the needs of your game.
+@export var toggle_keybind: Shortcut
+
+@onready var node_tree: Tree = $NodeTree
+@onready var node_info: TabContainer = $NodeInfo
+
+var node_open: bool # Checks if a Node has been selected for viewing or editing.
+var tree_items: Array[TreeItem] # Helps keep track of where the items are positioned
+var tree_string: String
+
+func _ready() -> void:
+	get_tree().node_added.connect(_on_node_added)
+	get_tree().node_removed.connect(_on_node_removed)
+	hide()
+
+	tree_string = get_tree().root.get_tree_string()
+	node_tree.clear()
+	tree_items.clear()
+
+	# Setup connections to the nodes
+	var node_list: Array = tree_string.split("\n")
+	node_list.resize(node_list.size() - 1) # Remove empty space
+
+	for node in node_list:
+		if node == ".": # This is always the root
+			create_tree_item(node)
+		else:
+			var path: Array = node.split("/")
+			var root: String = path.back()
+			if path.size() > 1:
+				root = node.left(-("/" + path.back()).length())
+			create_tree_item("/root/" + node, tree_items.filter(filter_by_tooltip.bind("/root/" + root)).front() if path.size() > 1 else node_tree.get_root(), node_list.find(root))
+
+func _input(event: InputEvent) -> void:
+	if toggle_keybind.matches_event(event) and event.is_pressed():
+		toggle()
+
+func toggle():
+	if visible: hide()
+	else: show()
+
+# Function to search by tooltip since that's the full path we'll use to search for nodes and sort the tree
+func filter_by_tooltip(item: TreeItem, path: String):
+	return item.get_tooltip_text(0) == path
+
+# Remove EVERY Null instance
+func remove_null_instances():
+	for i in range(tree_items.size() - 1, -1, -1):
+		var item: TreeItem = tree_items[i]
+		if not is_instance_valid(item):
+			tree_items.remove_at(i)
+
+func create_tree_item(path: String, parent: TreeItem = null, index: int = -1):
+	if path == ".": # This is always the root
+		var root: TreeItem = node_tree.create_item()
+		root.set_text(0, "Window")
+		root.set_tooltip_text(0, "/root")
+		root.set_suffix(0, "- root")
+		tree_items.append(root)
+	else:
+		var node: Node = get_node(path)
+		var item: TreeItem = node_tree.create_item(parent, index)
+
+		item.collapsed = true
+		item.set_text(0, node.get_class())
+		var rename_item = func(n: Node):
+			item.set_tooltip_text(0, n.get_path())
+			item.set_suffix(0, "- " + n.name)
+
+		rename_item.call(node)
+		node.renamed.connect(rename_item.bind(node))
+
+		# I would've done a MATCH here, but I don't know how to do that for getting Node super classes
+		# Also I would've used icons, but the Editor icons aren't built-in
+		if node is Node2D:
+			item.set_custom_color(0, Color.ROYAL_BLUE)
+		elif node is Node3D:
+			item.set_custom_color(0, Color.INDIAN_RED)
+		elif node is Control:
+			item.set_custom_color(0, Color.LIME_GREEN)
+
+		tree_items.insert(index, item)
+
+func remove_tree_item(node: Node):
+	var item: TreeItem = tree_items.filter(filter_by_tooltip.bind(node.get_path())).front()
+	tree_items.erase(item)
+	item.free()
+	tree_string = get_tree().root.get_tree_string()
+
+func _on_node_added(node: Node):
+	if tree_items.filter(filter_by_tooltip.bind(node.get_path())).is_empty():
+		remove_null_instances()
+		tree_string = get_tree().root.get_tree_string()
+		var node_list: Array = tree_string.split("\n")
+		node_list.resize(node_list.size() - 1)
+
+		var path: Array = (node.get_path() as String).right(-"/root/".length()).split("/")
+		var root: String = path.back()
+		if path.size() > 1:
+			root = (node.get_path() as String).right(-"/root/".length()).left(-("/" + path.back()).length())
+		create_tree_item(node.get_path(), tree_items.filter(filter_by_tooltip.bind("/root/" + root)).front() if path.size() > 1 else node_tree.get_root(), node_list.find(root))
+
+func _on_node_removed(node: Node):
+	if get_tree() == null: return # This means the Window is closed
+	if tree_items.filter(filter_by_tooltip.bind(node.get_path())).size() > 0:
+		remove_tree_item(node)
